697cbd5ad1761ea39884d92e559c132e4cf3fc8f
Marius Dumitru Florea <marius@xwiki.com>
2016-09-06 18:12:19 +0300

    [misc] Don't stop at the first conversion failure during test (we want to see all the failures).

--- jodconverter-core/src/test/java/org/artofsolving/jodconverter/OfficeDocumentConverterFunctionalTest.java
+++ jodconverter-core/src/test/java/org/artofsolving/jodconverter/OfficeDocumentConverterFunctionalTest.java
@@ -18,12 +18,15 @@ import static org.testng.Assert.assertTrue;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.io.FilenameUtils;
 import org.artofsolving.jodconverter.document.DocumentFormat;
 import org.artofsolving.jodconverter.document.DocumentFormatRegistry;
 import org.artofsolving.jodconverter.office.DefaultOfficeManagerConfiguration;
+import org.artofsolving.jodconverter.office.OfficeException;
 import org.artofsolving.jodconverter.office.OfficeManager;
 import org.testng.annotations.Test;
 
@@ -34,7 +37,6 @@ public class OfficeDocumentConverterFunctionalTest
     {
         OfficeManager officeManager = new DefaultOfficeManagerConfiguration().buildOfficeManager();
         OfficeDocumentConverter converter = new OfficeDocumentConverter(officeManager);
-        DocumentFormatRegistry formatRegistry = converter.getFormatRegistry();
 
         officeManager.start();
         try {
@@ -46,24 +48,40 @@ public class OfficeDocumentConverterFunctionalTest
                     return !name.startsWith(".");
                 }
             });
+            List<OfficeException> failedConversions = new ArrayList<OfficeException>();
             for (File inputFile : files) {
-                String inputExtension = FilenameUtils.getExtension(inputFile.getName());
-                DocumentFormat inputFormat = formatRegistry.getFormatByExtension(inputExtension);
-                assertNotNull(inputFormat, "unknown input format: " + inputExtension);
-                Set<DocumentFormat> outputFormats = formatRegistry.getOutputFormats(inputFormat.getInputFamily());
-                for (DocumentFormat outputFormat : outputFormats) {
-                    File outputFile = File.createTempFile("test", "." + outputFormat.getExtension());
-                    outputFile.deleteOnExit();
-                    System.out.printf("-- converting %s to %s... ", inputFormat.getExtension(), outputFormat
-                        .getExtension());
-                    converter.convert(inputFile, outputFile, outputFormat);
-                    System.out.printf("done.\n");
-                    assertTrue(outputFile.isFile() && outputFile.length() > 0);
-                    // TODO use file detection to make sure outputFile is in the expected format
-                }
+                failedConversions.addAll(convert(converter, inputFile));
+            }
+            if (!failedConversions.isEmpty()) {
+                throw failedConversions.get(0);
             }
         } finally {
             officeManager.stop();
         }
+    }
+
+    private List<OfficeException> convert(OfficeDocumentConverter converter, File inputFile) throws IOException
+    {
+        List<OfficeException> failedConversions = new ArrayList<OfficeException>();
+        String inputExtension = FilenameUtils.getExtension(inputFile.getName());
+        DocumentFormatRegistry formatRegistry = converter.getFormatRegistry();
+        DocumentFormat inputFormat = formatRegistry.getFormatByExtension(inputExtension);
+        assertNotNull(inputFormat, "unknown input format: " + inputExtension);
+        Set<DocumentFormat> outputFormats = formatRegistry.getOutputFormats(inputFormat.getInputFamily());
+        for (DocumentFormat outputFormat : outputFormats) {
+            File outputFile = File.createTempFile("test", "." + outputFormat.getExtension());
+            outputFile.deleteOnExit();
+            System.out.printf("-- converting %s to %s... ", inputFormat.getExtension(), outputFormat.getExtension());
+            try {
+                converter.convert(inputFile, outputFile, outputFormat);
+                System.out.printf("done.\n");
+                assertTrue(outputFile.isFile() && outputFile.length() > 0);
+                // TODO use file detection to make sure outputFile is in the expected format
+            } catch (OfficeException e) {
+                System.out.printf("failed.\n");
+                failedConversions.add(e);
+            }
+        }
+        return failedConversions;
     }
 }
